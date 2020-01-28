2caffa5a30ce8d76f459e4f737300442ce0e710c
sk4zuzu <sk4zuzu@gmail.com>
2020-01-29 00:05:21 +0100

    packer: making apt-get non-interactive

--- packer/utl/remote-exec/03-haproxy.sh
+++ packer/utl/remote-exec/03-haproxy.sh
@@ -3,6 +3,8 @@
 policy_rc_d_disable() (echo "exit 101" >/usr/sbin/policy-rc.d && chmod a+x /usr/sbin/policy-rc.d)
 policy_rc_d_enable()  (echo "exit 0"   >/usr/sbin/policy-rc.d && chmod a+x /usr/sbin/policy-rc.d)
 
+export DEBIAN_FRONTEND=noninteractive
+
 set -o errexit -o nounset -o pipefail
 set -x
 
